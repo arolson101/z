af7f8426a39aa3af2cbbb3af7fea451ec161bd44
arolson101 <andrew@olsononline.org>
2015-11-20 16:39:16 -0600

    add rx typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -6,3 +6,4 @@
 /// <reference path="redux-thunk/redux-thunk.d.ts" />
 /// <reference path="es6-collections/es6-collections.d.ts" />
 /// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="rx/rx.d.ts" />
