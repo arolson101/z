987b9ad37435fa8642728d73b221324b2c7cff62
arolson101 <andrew@olsononline.org>
2015-12-03 09:48:18 -0600

    fix router

--- src/components/component.tsx
+++ src/components/component.tsx
@@ -6,6 +6,6 @@ import * as React from "react";
 // stateless component
 export class Component<P> extends React.Component<P, any> {
 	shouldComponentUpdate(nextProps: P, nextState: any, nextContext: any): boolean {
-		return this.props === nextProps;
+		return this.props !== nextProps || this.state !== nextState || this.context !== nextContext;
 	}
 }
